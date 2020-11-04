70d7df26cf7223a851acd6ee954f6c815a83e177
Ilija Jolevski <ilija.jolevski@gmail.com>
2020-11-04 11:31:59 +0100

    Initial commit

---
+++ src/fikt/edu/mk/Main.java
@@ -0,0 +1,39 @@
+package fikt.edu.mk;
+
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        Covek ilija = new Covek(15, "Ilija", "Jol");
+        Covek c = new Covek();
+        c.setPrezime("prex");
+        c.setIme("im");
+        c.setGodini(2);
+
+        System.out.println("Sporedba: " + ilija.toString().indexOf("drug str"));
+        //StringTokenizer str = new StringTokenizer(ilija.toString());
+        StringBuffer strbuf = new StringBuffer();
+
+        strbuf.append(4);
+        strbuf.append("ilija");
+        strbuf.append(4.7);
+        strbuf.append(true);
+        System.out.println("strbuf: " + strbuf.toString());
+        System.out.println(ilija);
+        System.out.println(c);
+
+
+        //Nizi od objekti
+        Covek [] nizaOdLugje;
+        nizaOdLugje = new Covek[3];
+        nizaOdLugje[0] = ilija;
+        nizaOdLugje[1] = c;
+        nizaOdLugje[2] = new Covek(21,"nov", "objekt");
+        System.out.println(nizaOdLugje[2].toString());
+
+
+    }
+}
